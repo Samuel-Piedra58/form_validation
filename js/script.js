e250e4e71c3b2b0463ca7a05d67a51e185df5e5a
Samuel Piedra <samuel58piedra@gmail.com>
2019-05-26 23:42:54 -0700

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -44,9 +44,41 @@ function hideColorDropDown(isHidden) {
   const colorLabel = colorDropDown.prev();
   if (isHidden) {
     colorDropDown.hide();
-    colorLabel.text("Please Select a T-Shirt theme");
+    colorLabel.text("Please select a T-shirt theme");
   } else {
     colorDropDown.show();
     colorLabel.text("Color:");
   }
+}
+
+// using the parent node is a challenge. The event will bubble/delegate to unexpected objects when using the parent node attribute.
+
+$("fieldset.activities").on("change", 'input[type="checkbox"]', function(e) {
+  const text = e.target.parentNode.textContent;
+  console.log(extractEventDetail(text));
+});
+
+function extractEventDetail(string) {
+  const event_regex = /^(\D*)\s—\s(Tuesday|Wednesday)?\s?(\d\d?)?(am|pm)?-?(\d\d?)?(am|pm)?,?\s?\$(\d{3})$/;
+  return updateEventTime({
+    event_title: string.replace(event_regex, "$1"),
+    event_day: string.replace(event_regex, "$2"),
+    event_start_time: parseInt(string.replace(event_regex, "$3")),
+    event_start_merid: string.replace(event_regex, "$4"),
+    event_end_time: parseInt(string.replace(event_regex, "$5")),
+    event_end_merid: string.replace(event_regex, "$6"),
+    event_cost: parseInt(string.replace(event_regex, "$7"))
+  });
+}
+
+function updateEventTime(obj) {
+  // calculate start time
+  if (obj.event_start_merid === "pm" && obj.event_start_time != "12") {
+    obj.event_start_time = obj.event_start_time + 12;
+  }
+  // calculate end time
+  if (obj.event_end_merid === "pm" && obj.event_end_time != "12") {
+    obj.event_end_time = obj.event_end_time + 12;
+  }
+  return obj;
 }
